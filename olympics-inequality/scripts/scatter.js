d91711105dc05367f9032ccc6bf9ac3241f2984b
“wkim31” <won_kyung_kim@brown.edu>
2024-04-25 16:57:06 -0400

    tooltip

--- olympics-inequality/scripts/scatter.js
+++ olympics-inequality/scripts/scatter.js
@@ -49,7 +49,9 @@ d3.csv("https://raw.githubusercontent.com/data1500/data1500.github.io/main/provi
             .style("fill", function (d) { return colors(d.avgHigh); } )
             .on("mouseover", function() {
                 d3.select(this)
-                    .attr("r", 20);
+                    .attr("r", 20)
+                d3.select("#myTooltip")
+                    .html(d.avgHigh);
             })
             .on("mouseout", function() {
                 d3.select(this)
