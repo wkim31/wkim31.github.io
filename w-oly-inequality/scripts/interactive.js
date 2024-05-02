6408eadd3b890928f6ef2b8661d75f0c3a14fd7f
“wkim31” <won_kyung_kim@brown.edu>
2024-05-02 12:23:39 -0400

    hover

--- w-oly-inequality/scripts/interactive.js
+++ w-oly-inequality/scripts/interactive.js
@@ -1,64 +1,39 @@
-// set the dimensions and margins of the graph
-let margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+//JS code to handle hover effect on the map
 
-// append the svg object to the body of the page
-let svg = d3.select("#myChart")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var mapImg = document.getElementById('olympic-map');
+    
+    
+    var tooltip = document.getElementById('tooltip');
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/data1500/data1500.github.io/main/providenceTemp.csv", function(data) {
+    // Define the medal counts
+    var medalCounts = {
+        'USA': 5002,
+        'Russia': 3188
+    };
 
-    // Add X axis
-    let x = d3.scaleLinear()
-        .domain([1945, 2023])
-        .range([ 0, width ]);
+    //Add mouseover event listener to the map image
+    mapImg.addEventListener('mouseover', function (event) {
+        //Get the country name from the alt attribute of the hovered image
+        var country = event.target.alt;
 
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        
+        if (medalCounts.hasOwnProperty(country)) {
+            // Position the tooltip
+            tooltip.style.top = (event.clientY + 10) + 'px';
+            tooltip.style.left = (event.clientX + 10) + 'px';
 
-    // Add Y axis
-    let y = d3.scaleLinear()
-        .domain([56, 66])
-        .range([ height, 0]);
+            
+            tooltip.textContent = country + ': ' + medalCounts[country] + ' medals';
 
-    svg.append("g")
-        .call(d3.axisLeft(y));
+            // Show the tooltip
+            tooltip.style.display = 'block';
+        }
+    });
 
-    let colors = d3.scaleLinear()
-        .domain([54, 66])
-        .range(["#1f78b4", "#e31a1c"]);
-
-    // Add dots
-    let dots = svg.append("g")
-        .selectAll("dot")
-        .data(data)
-        .enter()
-        .append("circle")
-            .attr("cx", function (d) { return x(d.year); } )
-            .attr("cy", function (d) { return y(d.avgHigh); } )
-            .attr("r", 8)
-            .style("fill", "#666666")
-            .style("opacity", 0.5)
-            .style("fill", function (d) { return colors(d.avgHigh); } )
-            .on("mouseover", function() {
-                d3.select(this)
-                    .attr("r", 20)
-                
-                let rounded = Math.round(d.avgHigh * 10)/10 + "°F";
-                
-                d3.select("#myTooltip")
-                    .html(rounded);
-            })
-            .on("mouseout", function() {
-                d3.select(this)
-                    .attr("r", 8);
-            });
-
-})
+    // Add mouseout event listener to hide the tooltip when mouse leaves the map image
+    mapImg.addEventListener('mouseout', function () {
+        tooltip.style.display = 'none';
+    });
+});
